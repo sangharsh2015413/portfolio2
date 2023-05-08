c01fd30dc524bbee777009bfe9ccc45d79f2c184
sangharsh2015413 <95182806+sangharsh2015413@users.noreply.github.com>
2023-05-08 17:49:36 +0530

    index added

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebaar').classList.toggle('sidebaarGo');
+    if(document.querySelector('.sidebaar').classList.contains('sidebaarGo')){
+        document.querySelector('.ham').style.display='inline';
+        document.querySelector('.cross').style.display='none';
+    }
+    else{
+        document.querySelector('.ham').style.display='none';
+        setTimeout(()=>{
+        document.querySelector('.cross').style.display='inline';
+    },300)
+    }
+})
